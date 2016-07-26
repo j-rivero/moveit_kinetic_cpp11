d5f4a623c5ebc6351abd9d1295551ce98c2b9b5b
Dave Coleman <davetcoleman@gmail.com>
2016-07-26 14:31:37 -0600

    add libccd-dev

--- run_before.sh
+++ run_before.sh
@@ -2,6 +2,7 @@
 
 echo "run_before script"
 apt-get -qq remove libfcl0 libfcl-dev ros-kinetic-octomap
+apt-get -qq install libccd-dev
 ls -a
 cd fcl
 wget https://raw.githubusercontent.com/ros-gbp/fcl-release/debian/jade/fcl/package.xml
