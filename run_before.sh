5232e632e73e59f70755d9261d23884ee43c2bcd
Dave Coleman <davetcoleman@gmail.com>
2016-07-26 14:40:21 -0600

    Added temp package.xml to repo for fcl

--- run_before.sh
+++ run_before.sh
@@ -2,9 +2,12 @@
 
 echo "run_before script"
 apt-get -qq remove libfcl0 libfcl-dev ros-kinetic-octomap
-apt-get -qq install libccd-dev
+#apt-get -qq install libccd-dev
 ls -a
-cd fcl
-wget https://raw.githubusercontent.com/ros-gbp/fcl-release/debian/jade/fcl/package.xml
-ls -a
-cd ../
+cp moveit_kinetic_cpp11/package.xml fcl/package.xml
+ls -a fcl
+
+# cd fcl
+# wget https://raw.githubusercontent.com/ros-gbp/fcl-release/debian/jade/fcl/package.xml
+# ls -a
+# cd ../
