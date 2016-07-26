fee9efc1789e5b56c18d16658101571fdd235e17
Dave Coleman <davetcoleman@gmail.com>
2016-07-26 14:28:36 -0600

    Debug folders

--- run_before.sh
+++ run_before.sh
@@ -2,6 +2,8 @@
 
 echo "run_before script"
 apt-get -qq remove libfcl0 libfcl-dev ros-kinetic-octomap
+ls -a
 cd fcl
 wget https://raw.githubusercontent.com/ros-gbp/fcl-release/debian/jade/fcl/package.xml
-../
+ls -a
+cd ../
