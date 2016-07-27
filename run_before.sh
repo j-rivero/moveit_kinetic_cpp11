e2dcd0b6cf7a3366210823076578a1aad01f71b3
Dave Coleman <davetcoleman@gmail.com>
2016-07-26 19:17:34 -0600

    Remove package.xml

--- run_before.sh
+++ run_before.sh
@@ -3,3 +3,6 @@
 echo "run_before script"
 apt-get -qq remove libfcl0 libfcl-dev ros-kinetic-octomap
 cp moveit_kinetic_cpp11/package.xml fcl/package.xml
+
+# Remove package.xml otherwise it will think there are multiple packages
+rm moveit_kinetic_cpp11/package.xml
