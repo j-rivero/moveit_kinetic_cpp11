9b649a5b015af4a971f5da21c3b45f829c9018e6
Dave Coleman <davetcoleman@gmail.com>
2016-07-26 19:16:48 -0600

    Cleanup output

--- run_before.sh
+++ run_before.sh
@@ -2,12 +2,4 @@
 
 echo "run_before script"
 apt-get -qq remove libfcl0 libfcl-dev ros-kinetic-octomap
-#apt-get -qq install libccd-dev
-ls -a
 cp moveit_kinetic_cpp11/package.xml fcl/package.xml
-ls -a fcl
-
-# cd fcl
-# wget https://raw.githubusercontent.com/ros-gbp/fcl-release/debian/jade/fcl/package.xml
-# ls -a
-# cd ../
